b035fed886b8df25aa5f23851aad262bedf7fa31
Bernhard <e0828592@student.tuwien.ac.at>
2015-09-23 04:25:35 +0200

    initialized orientationtracker plugin

---
+++ Lifestage/Assets/Scripts/VibrationProvider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VibrationProvider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void CalculateVibrationPattern(float distance)
+    {
+        // TODO Calculate Vibro Pattern and send to Service
+    }
+}
